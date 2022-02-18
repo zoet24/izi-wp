0b351d34bca835acd56d50d799a81ed1711f2135
Zoe Thexton <zoe.thexton@appeal.marketing>
2022-02-18 19:04:51 +0000

    Blocks: Partial update of hamburger header functionality

--- wp-content/themes/zoepix/components/global/header.php
+++ wp-content/themes/zoepix/components/global/header.php
@@ -1,20 +1,38 @@
+<?php
+    $logo_mobile = carbon_get_theme_option( 'header_logo_mobile' );
+    $logo_desktop = carbon_get_theme_option( 'header_logo_desktop' );
+?>
+
 <header class="header">
-    <h1>Header</h1>
-    <nav class="header__navbar">
-        <?php wp_nav_menu([
-                'menu' => 'Other Menu',
-                'menu_class' => 'header__navbar--links',
-                'menu_id' => 'header__navbar',
-                'container' => 'div',
-                'container_class' => 'header__navbar--nav',
-                'walker' => new Zoepix_Header_Walker()
-            ]);
-        ?>
-    </nav>
-    <div class="header__navbar hamburger" aria-label="Toggle menu">
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
+    <div class="header__container">
+        <button class="header__hamburger" aria-label="Toggle menu">
+            <div class="hamburger">
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+        </button>
+        <nav class="header__navbar">
+            <?php wp_nav_menu([
+                    'menu' => 'Main Menu',
+                    'menu_class' => 'header__navbar--links',
+                    'menu_id' => 'header__navbar',
+                    'container' => 'div',
+                    'container_class' => 'header__navbar--nav',
+                    'walker' => new Zoepix_Header_Walker()
+                ]);
+            ?>
+        </nav>
+        <?php if ($logo_mobile) : ?>
+            <a href="/zoepix/" class="header__logo header__logo--mobile">
+                <img src="<?php echo wp_get_attachment_image_url($logo_mobile, 'large'); ?>" alt="Zoepix logo">
+            </a>
+        <?php endif; ?>
+        <?php if ($logo_desktop) : ?>
+            <a href="/zoepix/" class="header__logo header__logo--desktop">
+                <img src="<?php echo wp_get_attachment_image_url($logo_desktop, 'large'); ?>" alt="Zoepix logo">
+            </a>
+        <?php endif; ?>
     </div>
 </header>
